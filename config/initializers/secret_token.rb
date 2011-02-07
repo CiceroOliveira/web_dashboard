e1c35d0394d17ce3a9cd139cf5c227ff95f9f2f4
Cicero Oliveira <cicero@cicerooliveira.com>
2011-02-07 00:38:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebDashboard::Application.config.secret_token = 'f368878d537c7e09178df3231c6c736a95a0410784eb7370dfe053fc830a4ae7fa2210316643b2cd9b80ac25d06f8976778f2a34c30597b6f8fa8a1feafa9947'
